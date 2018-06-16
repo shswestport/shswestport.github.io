01105493724f94a01051509c77c4857da95721c2
ArrangingErmine <eoghanmmurphy@gmail.com>
2018-06-16 21:09:40 +0100

    Changed slide. Removed JS logs

--- script.js
+++ script.js
@@ -14,12 +14,10 @@ carousel();
 function carousel() {
     var i;
     var x = document.getElementsByClassName("mySlides");
-    console.log(x);
     for (i = 0; i < x.length; i++) {
        x[i].style.display = "none";
     }
     myIndex++;
-    console.log(x[myIndex-1]);
     if (myIndex > x.length) {myIndex = 1}
     x[myIndex-1].style.display = "block";
     setTimeout(carousel, 4000);
